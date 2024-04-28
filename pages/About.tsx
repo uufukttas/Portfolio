723e7790596e5e951c752807d95685f684c5f348
uufukttas <uufukttas@hotmail.com>
2024-04-28 23:23:39 +0300

    Added pages

---
+++ pages/About.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div className="about-page-container flex h-screen bg-black text-white">
+            <div>
+                asd
+            </div>
+        </div>
+    );
+};
+
+export default About;
