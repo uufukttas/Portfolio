38a1bdbc744cb8d79c020bea7e4ebf7be7e91491
uufukttas <uufukttas@hotmail.com>
2024-05-05 14:03:44 +0300

    Added font family to the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "uufukttas.",
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html className={montserrat.className} lang="en">
       <body>{children}</body>
     </html>
   );
