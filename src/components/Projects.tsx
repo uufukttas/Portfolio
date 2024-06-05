4781f85bfc6901f2ad364ed3ef84c0c7a77d1d7f
uufukttas <uufukttas@hotmail.com>
2024-06-05 23:33:00 +0300

    refactor: Update Projects component layout and styling

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -24,7 +24,7 @@ const ProjectsPage: React.FC = () => {
         <div className="container py-12 mx-auto h-full">
             <div className="flex mx-auto flex-wrap justify-center">
                 {projects.map((project) => (
-                    <div key={project.id} className="h-auto w-1/3 flex justify-center">
+                    <div key={project.id} className="h-auto w-full md:w-1/3 flex justify-center p-4 md:p-0">
                         <div className="rounded-lg shadow-lg h-full text-center my-2">
                             <Link href={project.link}>
                                 <Image alt="Project" className="hover:scale-110" height={200} src={project.image} width={300} />
